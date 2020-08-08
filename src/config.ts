09c62aaa859d056999da4f7c006469f43a2bd079
d1sc0 <ilovediscostu@gmail.com>
2020-08-08 21:08:33 +0100

    updated config

--- src/config.ts
+++ src/config.ts
@@ -14,13 +14,18 @@ export interface Proxy extends Options {
 }
 
 const config: Config = {
-  allowedDomains: ['http://localhost:5500', 'http://127.0.0.1:5500', '*'],
+  allowedDomains: ['http://localhost:5500'],
   proxies: [
     {
       route: '/quote',
       allowedMethods: ['GET'],
-      target:
-        'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json',
+      allowedDomains: ['*'],
+      target: 'http://api.forismatic.com/api/1.0',
+      queryparams: {
+        method: 'getQuote',
+        lang: 'en',
+        format: 'json',
+      },
     },
   ],
 };
