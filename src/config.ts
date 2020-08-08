4c224fd326b70a7f85ae79a6760425a4fe1b5ea0
d1sc0 <ilovediscostu@gmail.com>
2020-08-08 20:59:28 +0100

    config update

--- src/config.ts
+++ src/config.ts
@@ -22,31 +22,6 @@ const config: Config = {
       target:
         'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json',
     },
-    {
-      route: '/weather',
-      allowedMethods: ['GET'],
-      target: 'https://api.openweathermap.org/data/2.5/weather',
-      queryparams: {
-        appid: process.env.WEATHER_API_KEY,
-      },
-    },
-    {
-      route: '/ipinfo',
-      allowedMethods: ['GET'],
-      target: 'https://ipinfo.io/',
-      queryparams: {
-        token: process.env.IPINFO_TOKEN,
-      },
-    },
-    {
-      route: '/github',
-      allowedMethods: ['GET'],
-      target: 'https://api.github.com',
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `Token ${process.env.GITHUB_TOKEN}`,
-      },
-    },
   ],
 };
 
