8ba970a638a488518b12f3f35920da4c53f9a78b
d1sc0 <ilovediscostu@gmail.com>
2020-08-09 17:26:25 +0100

    updating whitelist

--- routes/airquote.js
+++ routes/airquote.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // set dynamic CORS whitelist
-var whitelist = ['*', 'http://localhost', 'http://localhost:5500'];
+var whitelist = ['http://localhost', 'http://localhost:5500'];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
