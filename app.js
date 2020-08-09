10caa039d593815f19b2c6241a88db098926cf4d
d1sc0 <ilovediscostu@gmail.com>
2020-08-09 12:42:48 +0100

    update quote route

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ const request = require('request');
 
 const app = express();
 
+// set origin headers for CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost');
   next();
 });
 
-app.get('/quote', (req, res) => {
+// route for jokes API
+app.get('/joke', (req, res) => {
   request(
     { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//route for quote API
+app.get('/quote', (req, res) => {
+  request(
+    {
+      url:
+        'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json',
+    },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
