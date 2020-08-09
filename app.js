e1b4f2003743d559fd2e17b680b43a935bc97605
d1sc0 <ilovediscostu@gmail.com>
2020-08-09 13:12:21 +0100

    updating with dynamic whitelist

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 const request = require('request');
 
 const app = express();
 
-// set origin headers for CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5500');
-  next();
-});
+// set dynamic CORS whitelist
+var whitelist = ['http://localhost', 'http://localhost:5500'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 // route for jokes API
-app.get('/joke', (req, res) => {
+app.get('/joke', cors(corsOptions), (req, res, next) => {
   request(
     { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-
       res.json(JSON.parse(body));
     }
   );
 });
 
 //route for quote API
-app.get('/quote', (req, res) => {
+app.get('/quote', cors(corsOptions), (req, res, next) => {
   request(
     {
       url:
